ca9711bf1c9cac954d47728a6bf0d4e3d900e581
Amirgol00 <amir.golshan.al@gmail.com>
2023-12-07 15:59:23 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "643767aa393d810e29c27bdf65ff21ee
-"
+weather_api_key = "416d0a057c8542da29b4fc235ffd0c79"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "30c3bb5166cc4b0dbc1bc0308240e0d2"
